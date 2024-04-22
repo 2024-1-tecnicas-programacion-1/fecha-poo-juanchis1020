67c55b11827c1feb07eff20d3e3503148c1b730c
Juanchis <juansega96@gmail.com>
2024-04-21 19:34:10 -0500

    Código
    Se escribió el código para con la realización del taller

--- src/main/java/com/mycompany/fecha/poo/Fecha.java
+++ src/main/java/com/mycompany/fecha/poo/Fecha.java
@@ -1,5 +1,59 @@
 package com.mycompany.fecha.poo;
+import java.util.HashMap;
 
 public class Fecha {
-    // TODO: Aquí va el código de la clase fechas
+    private int dia;
+    private int mes;
+    private int annio;
+    
+    private HashMap<Integer, String> meses;
+        
+    public Fecha(int dia, int mes, int annio){ //Complejidad temporal: O(1) Tiempo constante.
+        this.dia = dia;
+        this.mes = mes;
+        this.annio = annio;
+        this.meses = new HashMap<>();
+        meses.put(1, "enero");
+        meses.put(2, "febrero");
+        meses.put(3, "marzo");
+        meses.put(4, "abril");
+        meses.put(5, "mayo");
+        meses.put(6, "junio");
+        meses.put(7, "julio");
+        meses.put(8, "agosto");
+        meses.put(9, "septiembre");
+        meses.put(10, "octubre");
+        meses.put(11, "noviembre");
+        meses.put(12, "diciembre");
+    }
+    
+    public String fechaCorta(){ //Complejidad temporal: O(1) Tiempo constante.
+        return dia+"/"+mes+"/"+annio;
+    }
+    public boolean validarFecha(){
+        boolean validacion = true;
+        if(mes>=1 && mes<=12){ //Complejidad temporal: O(1) Tiempo constante.
+            if(dia>0 || dia<=31){
+                if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia>30){
+                    validacion = false;
+                }
+                else if(mes == 2 && ((annio % 4 == 0 && dia > 29) || (annio % 4 != 0 && dia > 28))){
+                    validacion = false;
+                }
+            }
+            else{
+                validacion = false;
+            }
+        }
+        else{
+            validacion = false;
+         }
+        return validacion;
+    }
+    public String mesLetra(){ //Complejidad temporal: O(1) Tiempo constante.
+        return meses.get(mes);
+    }
+    public String fechaLarga(){ //Complejidad temporal: O(1) Tiempo constante.
+        return dia+" de "+ meses.get(mes) +" de "+annio;
+    }
 }
--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,26 @@
 package com.mycompany.fecha.poo;
+import java.util.Scanner;
 
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+    public static void main(String[] args) { //Complejidad temporal: O(1) Tiempo constante.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese una fecha:");
+        System.out.println("Día: ");
+        int dia = sc.nextInt();
+        System.out.println("Mes: ");
+        int mes = sc.nextInt();
+        System.out.println("Año: ");
+        int annio = sc.nextInt();
+        Fecha Fecha1 = new Fecha(dia, mes, annio);
+        if (Fecha1.validarFecha()==true){
+            System.out.println("La fecha es válida:");
+        }
+        else{
+            System.out.println("La fecha es inválida.");
+        }
+        if (Fecha1.validarFecha()==true){
+            System.out.println(Fecha1.fechaLarga());
+        }
+        
+    }
 }
